3a6dd2a08d4c61cca55a37ed5b831ebdf9e6d1c6
Ahmed Elattary <ahmedelattary@gmail.com>
2023-02-09 16:04:55 +0200

    adding file upload capability

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const fileupload = require("express-fileupload");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(fileupload());
+app.use(express.static("files"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = 1234;
 
 const Pool = require("pg").Pool;
@@ -19,6 +24,7 @@ const command =
   "shp2pgsql -s 2100 -d /home/enomix/www/EsoterikoFinal/Maps/shps/nodes.shp  | psql -h localhost -p 5432 -d esoteriko -U postgres";
 
 app.get("/", (req, res) => {
+  console.log(__dirname);
   pool.query("SELECT * FROM test", (error, results) => {
     if (error) {
       throw error;
@@ -26,7 +32,18 @@ app.get("/", (req, res) => {
     res.status(200).json(results.rows);
   });
 });
+app.post("/upload", (req, res) => {
+  const newpath = __dirname + "/uploads/";
+  const file = req.files.file;
+  const filename = file.name;
 
+  file.mv(`${newpath}${filename}`, (err) => {
+    if (err) {
+      res.status(500).send({ message: "File upload failed", code: 200 });
+    }
+    res.status(200).send({ message: "File Uploaded", code: 200 });
+  });
+});
 app.listen(port, () => {
   console.log(`Listening on port ${port} 🌍`);
   console.log(Date.now());
